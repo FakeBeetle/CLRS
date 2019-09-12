6e2dff01d87010cd356d6d606ec93a898158b312
FakeBeetle <47056213+FakeBeetle@users.noreply.github.com>
2019-09-12 17:50:33 +0800

    random_version_of_quick_sort

--- C07-Quicksort/exercise_code/quick_sort.cpp
+++ C07-Quicksort/exercise_code/quick_sort.cpp
@@ -5,32 +5,47 @@ void swap(T* p1, T* p2) {
 	*p1 = *p2;
 	*p2 = intermedia;
 }
+
+
 template <typename T>
 int partition(T A[], int start, int end) {
 	int standard = A[end];
-	int count_of_equal_numbers = 0;
+	int count_equal_numbers = 0;
 	int current_small_index = 0;
-	for (int i = start; i != end; ++i) {
+	for (int i = 0; i != end; ++i) {
 		if (A[i] == A[end])
-			++count;//这个计数保证了在等于standard的元素较多时，能返回这些相同元素的中间位置下标
+			++count_equal_numbers;
 		if (A[i] <= A[end]) {
 			swap(&A[current_small_index], &A[i]);
 			++current_small_index;
 		}
 	}
 	swap(&A[current_small_index], &A[end]);
-	return current_small_index-count_of_equal_numbers/2;//
+	return current_small_index-count_equal_numbers/2;
 }
 
+template <typename T>
+int random_partition(T A[], int start, int end) {
+	int random_index = rand() % (end - start + 1) + start;
+	swap(&A[random_index], &A[end]);
+	return partition(A, start, end);
+}
 template <typename T>
 void quick_sort(T A[], int start, int end) {// n = end - start + 1
 	if (start >= end)
 		return;
-	int partition_point = partition(A, start, end);//cn
-	// α = (partition_point - start)/n
-	quick_sort(A, start, partition_point - 1);//αn
-	quick_sort(A, partition_point + 1, end);//(1-α)n
+	int partition_point = random_partition(A, start, end);//cn
+	// £\ = (partition_point - start)/n
+	quick_sort(A, start, partition_point - 1);//£\n
+	quick_sort(A, partition_point + 1, end);//(1-£\)n
+}
 
+int main() {
+	int A[7] = { 2, 5, 9, 3, 7, 0, -1 };
+	quick_sort(A, 0, 6);
+	for (int i = 0; i != 7; i++)
+		std::cout << A[i];
+	return 0;
 }
 /*快速排序的最好情况是
 		T(n) = T(q)+T(n-1-q) + θ(n)
